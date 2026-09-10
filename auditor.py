ab1ea23aa2bf5895fe2ef272c6cf693bfea26c1e
jinhao <2605675@sit.singaporetech.edu.sg>
2026-09-10 17:34:56 +0800

    Task 7

--- auditor.py
+++ auditor.py
@@ -3,14 +3,19 @@ user_input = ""
 
 while True:
     user_input = input("Enter a stock quantity: ")
-
+    print(user_input[1:])
     if user_input == "quit":
         break
 
     if user_input.isdigit():
         stock = int(user_input)
         inventory += stock
-    elif user_input.startswith('-') and user_input[1:].replace('-', '', 1).isdigit():
+        if inventory > 500:
+            print("Inventory exceed 500 units")
+            break
+        else:
+            continue
+    elif user_input.startswith("-") and user_input.replace("-", "", 1).isdigit():
         print("No negative number in user input")
         continue
     else:
