55f65b9149be1bc6de4897c5d768e4223de09ef8
jinhao <2605675@sit.singaporetech.edu.sg>
2026-09-18 09:25:13 +0800

    added get_valid_input()

--- lab_3/modular_auditor.py
+++ lab_3/modular_auditor.py
@@ -2,26 +2,36 @@ inventory = 0
 failed_entries = 0
 user_input = ""
 
-while True:
+def get_valid_input():
     user_input = input("Enter a stock quantity: ")
     if user_input == "quit":
-        print("Total Units Processed: ", inventory)
-        print("Number of Failed/Rejected Entries: ", failed_entries)
-        break
+        return user_input
+
 
     if user_input.isdigit():
-        stock = int(user_input)
-        inventory += stock
-        if inventory > 500:
-            print("Inventory exceed 500 units")
-            break
-        else:
-            continue
+        user_input = int(user_input)
+        return user_input
+
+
     elif user_input.startswith("-") and user_input.replace("-", "", 1).isdigit():
         print("No negative number in user input")
         failed_entries += 1
-        continue
+        get_valid_input()
+
     else:
         print("User input is not a integer")
         failed_entries += 1
-        continue
+        get_valid_input()
+
+    return
+
+while True:
+    valid_input = get_valid_input()
+    
+    # if valid_input == "quit":
+    #     print("Total Units Processed: ", inventory)
+    #     print("Number of Failed/Rejected Entries: ", failed_entries)
+
+    # inventory += stock
+    # if inventory > 500:
+    #     print("Inventory exceed 500 units")
